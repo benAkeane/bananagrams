a04e2799b8c6b82cbbef0187d05dbfcdda2b0a0e
benAkeane <benthelen88@gmail.com>
2025-12-11 14:48:37 -0500

    moved some files into new folders, created postgres db

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
 
 export default function App() {
     return (
