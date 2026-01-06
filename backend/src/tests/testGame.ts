1d7d8c69f115ffda0bb08e0e9a88f0a70c9712c2
benAkeane <benthelen88@gmail.com>
2026-01-06 14:29:31 -0500

    updated backend test file

--- backend/src/tests/testGame.ts
+++ backend/src/tests/testGame.ts
@@ -22,6 +22,40 @@ async function test() {
     console.log('Player board:', game.players['player1']!.board);
     console.log('Player rack:', game.players['player1']!.rack);
 
+    // test invalid word: "ABCDEFG"
+    game.players['player1']!.rack = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    const placements2 = [
+        { letter: 'A', x: 0, y: 1 },
+        { letter: 'B', x: 1, y: 1 },
+        { letter: 'C', x: 2, y: 1 },
+        { letter: 'D', x: 3, y: 1 },
+        { letter: 'E', x: 4, y: 1 }, 
+        { letter: 'F', x: 5, y: 1 },
+        { letter: 'G', x: 6, y: 1 }, 
+    ];
+    const result2 = await game.playTiles('player1', placements2);
+    console.log('Playing invald word "ABCDEFG":', result2);
+
+    // try to play a word without enough tiles
+    const placements3 = [
+        { letter: 'A', x: 0, y: 2 },
+        { letter: 'P', x: 1, y: 2 }, 
+        { letter: 'P', x: 2, y: 2 },
+        { letter: 'L', x: 3, y: 2 }, 
+        { letter: 'E', x: 4, y: 2 },
+    ];
+    const result3 = await game.playTiles('player1', placements3);
+    console.log('Playing "APPLE" without enough tiles:', result3);
+
+    // test peel (adds 1 tile to rack)
+    game.peel();
+    console.log('Player rack after peel:', game.players['player1']!.rack);
+
+    // Test dump (removes given tile, replaces with three new tiles)
+    const tileToDump = game.players['player1']!.rack[0]; // dump first tile
+    const result5 = game.dump('player1', tileToDump);
+    console.log(`Dumping tile "${tileToDump}":`, result5);
+    console.log('Player rack after dump:', game.players['player1']!.rack);
 }
 
 test();
